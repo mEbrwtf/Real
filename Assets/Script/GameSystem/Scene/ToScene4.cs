eade075a067328a58656dbd84ee10461e71fff6f
Jelly1314 <pisit.t@tripatschool.ac.th>
2024-09-23 22:00:07 +0700

    FadeInFadeOut

--- Assets/Script/GameSystem/Scene/ToScene4.cs
+++ Assets/Script/GameSystem/Scene/ToScene4.cs
@@ -1,17 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ToScene3 : MonoBehaviour
+public class ToScene4 : MonoBehaviour
 {
+    public Image black;
+    public Animator fade;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Scene 4");
+            StartCoroutine(Fading());
         }
+    }
+
+    IEnumerator Fading()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() =>black.color.a == 1);
+        SceneManager.LoadScene("Scene 4");
     }
     // Update is called once per frame
     void Update()
