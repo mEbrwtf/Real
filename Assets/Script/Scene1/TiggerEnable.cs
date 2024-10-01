20f459bc78d37af56a307c292b8fbd35b83378a0
Jelly1314 <pisit.t@tripatschool.ac.th>
2024-10-01 16:03:45 +0700

    elevator

--- Assets/Script/Scene1/TiggerEnable.cs
+++ Assets/Script/Scene1/TiggerEnable.cs
@@ -1,50 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Make sure to include this for UI elements
+using System.Collections.Generic;
 
-public class TiggerEnable : MonoBehaviour
-
+public class TriggerAudioManager : MonoBehaviour
 {
-    public AudioSource two;
-    public Collider triggerCollider;
-    public Collider NewtriggerCollider;
-    // Start is called before the first frame update
+    public List<AudioSource> audioSources; // List of AudioSources to handle multiple audio clips
+    public List<string> subtitles; // List of subtitles corresponding to each audio clip
+    public List<float> delays; // List of delays corresponding to each audio clip
+    public Collider triggerCollider; // To disable the collider after the audio sequence
+    public static AudioSource currentPlayingAudio; // Tracks the currently playing audio
+    public int currentAudioIndex = 0; // Tracks the current audio in the list
+
+    [Header("UI Elements")]
+    public Text subtitleText; // Reference to the UI Text element for displaying subtitles
+
     void Start()
     {
-        triggerCollider.enabled = false;
+        triggerCollider = GetComponent<Collider>(); // Get the trigger collider
+        subtitleText.text = ""; // Clear the subtitle text at the start
     }
+
+    // When the player enters the trigger zone
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // Ensure the object is the player
         {
-            Debug.Log("triggerStart");
-            two.Play();
-            StartCoroutine(WaitForAudioToFinish());
+            PlayAudio(); // Start playing the audio sequence
         }
     }
-    IEnumerator WaitForAudioToFinish()
+
+    // Play the current audio
+    void PlayAudio()
     {
-        yield return new WaitUntil(() => !two.isPlaying); // Wait until the audio finishes
-        Debug.Log("audioIsntPlaying");
-        waiting();
+        // Stop the currently playing audio (if any)
+        if (currentPlayingAudio != null && currentPlayingAudio.isPlaying)
+        {
+            currentPlayingAudio.Stop();
+        }
+
+        // Play the current audio in the list
+        currentPlayingAudio = audioSources[currentAudioIndex];
+        currentPlayingAudio.Play();
+
+        // Display the corresponding subtitle
+        DisplaySubtitle(currentAudioIndex);
+
+        // Start a coroutine to wait for the audio to finish and play the next one
+        StartCoroutine(PlayNextAudioAfterCurrentEnds());
     }
 
-    void waiting()
+    // Display the subtitle for the current audio index
+    void DisplaySubtitle(int index)
     {
-        triggerCollider.enabled = false;
-        NewtriggerCollider.enabled = true;
-        Debug.Log("newtriggerStart");
-        two.Play();
-        // StartCoroutine(DelayedAction());
+        if (index < subtitles.Count) // Check if index is valid
+        {
+            subtitleText.text = subtitles[index]; // Display the corresponding subtitle
+        }
     }
 
-    /*private IEnumerator DelayedAction()
+    // Coroutine to wait for the audio to finish and then play the next one
+    private System.Collections.IEnumerator PlayNextAudioAfterCurrentEnds()
     {
-        yield return new WaitForSeconds(22f);
+        // Wait until the current audio clip is finished
+        yield return new WaitForSeconds(currentPlayingAudio.clip.length);
 
+        // Get the delay for the next audio from the list
+        float nextDelay = (currentAudioIndex < delays.Count) ? delays[currentAudioIndex] : 0f;
+
+        // Delay before playing the next audio
+        yield return new WaitForSeconds(nextDelay);
+
+        // Clear the current subtitle
+        subtitleText.text = "";
+
+        // Move to the next audio
+        currentAudioIndex++;
+
+        if (currentAudioIndex < audioSources.Count) // Check if there's a next audio
+        {
+            PlayAudio(); // Play the next audio
+        }
+        else
+        {
+            // Optionally disable the trigger collider here if you want to do so after the last audio
+            // triggerCollider.enabled = false;
+        }
     }
-    // Update is called once per frame
-    void Update()
+
+    // When the player exits the trigger zone
+    void OnTriggerExit(Collider other)
     {
-    }*/
+        if (other.CompareTag("Player")) // Ensure the object is the player
+        {
+            // Disable the collider immediately when the player exits
+            triggerCollider.enabled = false; // Disable the trigger collider
+        }
+    }
 }
