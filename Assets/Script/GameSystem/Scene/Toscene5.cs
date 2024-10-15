eaf1bb66d7c625e491460bfb122b88cd34efd2df
Jelly1314 <pisit.t@tripatschool.ac.th>
2024-10-15 16:11:03 +0700

    great

---
+++ Assets/Script/GameSystem/Scene/Toscene5.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ToScene5 : MonoBehaviour
+{
+    public Image black;
+    public Animator fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("Scene 5");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
