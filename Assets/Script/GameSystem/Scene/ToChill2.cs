1da970343153e317becfe7bb738910662983aeb1
Jelly1314 <pisit.t@tripatschool.ac.th>
2024-12-30 15:52:07 +0700

    sawo

---
+++ Assets/Script/GameSystem/Scene/ToChill2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tochill2: MonoBehaviour
+{
+    public Image black;
+    public Animator fade;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("Chill2");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
