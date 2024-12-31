abefb4436a20faddc3d52007cb4d3e259a35b219
Jelly1314 <pisit.t@tripatschool.ac.th>
2024-12-31 14:45:18 +0700

    highlight

--- Assets/Script/Highlight/InteractHighlight.cs
+++ Assets/Script/Highlight/InteractHighlight.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class InteractHighlight : MonoBehaviour
 {
+    public GameObject unhighlight;
+    public GameObject highlight;
+    bool inReach;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inReach = false;
+        highlight.SetActive(false);
     }
-
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Reach"))
+        {
+            inReach = true;
+            highlight.SetActive(true);
+            unhighlight.SetActive(false);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Reach"))
+        {
+            inReach = false;
+            highlight.SetActive(false);
+            unhighlight.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (inReach && Input.GetButtonDown("USE"))
+        {
+            highlight.SetActive(false);
+            unhighlight.SetActive(true);
+        }
     }
 }
