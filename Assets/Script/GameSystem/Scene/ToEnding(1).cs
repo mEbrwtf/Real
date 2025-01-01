e24764a8d9b83bab9210ebf075618f16a4c795a6
Jelly1314 <pisit.t@tripatschool.ac.th>
2025-01-01 17:14:20 +0700

    sigh

--- Assets/Script/GameSystem/Scene/ToEnding(1).cs
+++ Assets/Script/GameSystem/Scene/ToEnding(1).cs
@@ -18,13 +18,23 @@ public class ToEnding : MonoBehaviour
         {
             StartCoroutine(Fading());
         }
+        else if (other.CompareTag("Super Player"))
+        {
+            StartCoroutine(Fading2());
+        }
     }
 
     IEnumerator Fading()
     {
         fade.SetBool("Fade", true);
         yield return new WaitUntil(() => black.color.a == 1);
-        SceneManager.LoadScene("Ending(1)");
+        SceneManager.LoadScene("Scene 1");
+    }
+    IEnumerator Fading2()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("ToEnding(1)");
     }
     // Update is called once per frame
     void Update()
