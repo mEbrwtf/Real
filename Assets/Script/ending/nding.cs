fe469616095cb636d3c7ddb09693fdc7d6c48320
Jelly1314 <pisit.t@tripatschool.ac.th>
2025-01-26 18:14:42 +0700

    rest

--- Assets/Script/ending/nding.cs
+++ Assets/Script/ending/nding.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class nding : MonoBehaviour
 {
+    public GameObject Ending;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Ending.SetActive(true);
     }
 
     // Update is called once per frame
