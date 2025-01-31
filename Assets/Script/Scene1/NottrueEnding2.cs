487df56060241bd750457f6c7fa48dd19e5d5f72
Jelly1314 <pisit.t@tripatschool.ac.th>
2025-01-31 21:15:45 +0700

    sijf

--- Assets/Script/Scene1/NottrueEnding2.cs
+++ Assets/Script/Scene1/NottrueEnding2.cs
@@ -65,7 +65,7 @@ public class NottrueEnding2 : MonoBehaviour
 
     private IEnumerator RingDuration()
     {
-        yield return new WaitForSeconds(19f);
+        yield return new WaitForSeconds(25f);
         ring.Play();
         pickup.enabled = true;
         yield return new WaitForSeconds(32f);
