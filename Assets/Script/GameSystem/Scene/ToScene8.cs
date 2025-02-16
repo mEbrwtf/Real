bd62c1559e9600a45b84c86bc9cc72105a8beaa4
Jelly1314 <pisit.t@tripatschool.ac.th>
2025-02-16 23:32:55 +0700

    cmon

---
+++ Assets/Script/GameSystem/Scene/ToScene8.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ToScene8 : MonoBehaviour
+{
+    public Image black;
+    public Animator fade;
+    public bool isNextScene = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("Scene 1");
+    }
+}
---
+++ Assets/Script/GameSystem/Scene/ToScenemenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ToScenemenu : MonoBehaviour
+{
+    public Image black;
+    public Animator fade;
+    public bool isNextScene = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading()
+    {
+        fade.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
